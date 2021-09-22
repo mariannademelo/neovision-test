26b9ce6372fd3457e29facb24580b6969d7c42a1
Marianna de Melo <alvesmariaanna@gmail.com>
2021-09-22 09:56:08 -0300

    A minor change

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ const form = document.querySelector("form")
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    const apiURL = "https://85k7bv04na.execute-api.us-east-1.amazonaws.com/dev/register"
+    const apiURI = "https://85k7bv04na.execute-api.us-east-1.amazonaws.com/dev/register"
     const token = "mariaannaalves@hotmail.com"
 
     const userName = form.name.value.trim()
     const userEmail = form.email.value.trim()
 
-    fetch(apiURL, {
+    fetch(apiURI, {
         method: 'POST',
         headers: { 
             "Authorization": token,
